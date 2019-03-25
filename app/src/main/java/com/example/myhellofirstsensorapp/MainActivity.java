ad9f411cc7bbebbd2369e4199a7f29076bfaf2f2
niklashedstrom <karlniklashedstrom@outlook.com>
2019-03-25 23:47:56 +0100

    added compass functionality

--- app/src/main/java/com/example/myhellofirstsensorapp/MainActivity.java
+++ app/src/main/java/com/example/myhellofirstsensorapp/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.myhellofirstsensorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myhellofirstsensorapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, compass.class);
+        startActivity(intent);
     }
 }
